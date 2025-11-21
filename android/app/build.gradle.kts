34be4fc2ebd9f4bc7a8905a6dddaff7c4407cf62
Amay Jha <83952941+AMAYKJHA@users.noreply.github.com>
2025-11-21 16:35:22 +0545

    fix: Updated android ndk version to 29.0.14206865

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 android {
     namespace = "com.example.citysewa"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    ndkVersion = "29.0.14206865"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
